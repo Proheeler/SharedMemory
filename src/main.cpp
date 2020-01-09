e8a19af4007d21bd92786412fbdfcad8060d0b70
v.reshetnikov <skemobob@gmail.com>
2020-01-09 23:06:52 +0300

    Added possibility of reiniting existing shared memory segment for new chunksize

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,20 @@ int main()
 
     // w h  chz 200 frames
 
-//    int shmid =Create(1920*1080*250,1920*1080);
+    //    int shmid =Create(1920*1080*250,1920*1080);
 
-//    my_array arr;
-//   arr = FindSegments();
-////   std::cout<<arr.size<<std::endl;
-//   for(int i=0;i<arr.size;++i)
-//   {
-//       std::cout<<i<<" "<<*(arr.pointer+i)<<std::endl;
-//   }
-//   FreeResultOfFindSegments(arr.pointer);
-////   delete []arr.pointer;
-//  //  int shmid_vid =Create(720*486*1*200+1600,720*486);
+    //    my_array arr;
+    //   arr = FindSegments();
+    ////   std::cout<<arr.size<<std::endl;
+    //   for(int i=0;i<arr.size;++i)
+    //   {
+    //       std::cout<<i<<" "<<*(arr.pointer+i)<<std::endl;
+    //   }
+    //   FreeResultOfFindSegments(arr.pointer);
+    ////   delete []arr.pointer;
+    //  //  int shmid_vid =Create(720*486*1*200+1600,720*486);
 
-    int shmid =Create(sizeof(int)*10+70,1);
+    int shmid =Create(sizeof(int)*10+100,4);
     BaseShmm* shmm = BaseShmm_Create(shmid);
     int a[]={1,2,3,4,5,6,7,8,9,0};
     for(int i=0;i<10;++i)
@@ -39,19 +39,24 @@ int main()
     {
         std::cout<<*(static_cast<int*>(i))<<std::endl;
     }
-////    std::cout<<shmm->getBlockSize()<<"\n"<<shmm->getMemorySize()<<std::endl;
-//    char a[]="1234567890";
-//    Write(shmm,0,a);
-//    std::string tmp((char*)(LockAndRead(shmm,0)));
 
-//    std::cout<<tmp<<std::endl;
-////    std::cout<<checkShmid(shmid)<<std::endl;
-////    BaseShmm_Delete(shmm);
-//    delete shmm;
+    std::cout<<shmm->getMaxSize()<<std::endl;
+    std::cout<<shmm->getBlockSize()<<"\t"<<shmm->getMemorySize()<<std::endl;
+    shmm->reinitMemory(16);
+    std::cout<<shmm->getMaxSize()<<std::endl;
+        std::cout<<shmm->getBlockSize()<<"\t"<<shmm->getMemorySize()<<std::endl;
+    //    char a[]="1234567890";
+    //    Write(shmm,0,a);
+    //    std::string tmp((char*)(LockAndRead(shmm,0)));
+
+    //    std::cout<<tmp<<std::endl;
+    ////    std::cout<<checkShmid(shmid)<<std::endl;
+    ////    BaseShmm_Delete(shmm);
+    //    delete shmm;
     BaseShmm::deleteSegment(shmid);
-////
+    ////
 
-   // std::cout<<"shm id video: "<<shmid_vid<<std::endl;
+    // std::cout<<"shm id video: "<<shmid_vid<<std::endl;
     return 0;
 }
 
